0e9fad75f9d17d07c7f2ba8b42788e6cf2c9fab1
Solancia <33269997+Solancia@users.noreply.github.com>
2019-09-04 00:44:40 -0700

    added comments to functions.js, increased abstraction
    maybe <3 :)

--- public/functions.js
+++ public/functions.js
@@ -1,32 +1,52 @@
+/*
+ * Function.js
+ *
+ * Used in app.js
+ *  -abstracts away app.js, but still has req and res to grab url request, might change that in main though.
+ */
+
 'use strict';
 
 const yelp = require('yelp-fusion');
 const API_KEY = "shiSLJAHIMJ0fUf9p2LtOCHHaygchm7KSMM-KmnXTS-hHQHFaEjPasuJgphfIOzJ-PVAs-bGo0Q_5Z2IfxhZxVOLneYu6Q1mwVYYn0co6GBU1E4zeduYgUnxCl9tXXYx";
 const client = yelp.client(API_KEY);
 
-//main function that interacts with app.js; calls on yelp-funsion to make a search, in which the JSON is sent to make_page()
-async function main(req,res) {
+
+//main function that interacts with app.js; calls on yelp-fusion to make a search, in which the JSON is sent to make_page()
+async function main(req, res) {
+    var user_requested = req.query.userRequest;
     client.search({
-        location: req.query.userRequest.location,
-        radius: Number(req.query.userRequest.distance),
-        price: req.query.userRequest.priceRange
+        location: user_requested.location,
+        radius: Number(user_requested.distance),
+        price: user_requested.priceRange
     }).then(response => {
-        make_page(req, res, response.jsonBody);
+        make_page(user_requested, res, response.jsonBody);
     }).catch(e => {
         console.log(e);
     });
 
 }
 
+
+//helper functions-------------------------------------------------------------------------
+
+
 //renders the page, add functions that calculate from response.jsonBody here
-function make_page(req, res, response) {
+function make_page(user_requested, res, response) {
     console.log();
-    //req.query.userRequest to send message
-    res.render("results", { location: req.query.userRequest.location,
-    radius: req.query.userRequest.distance, price: req.query.userRequest.priceRange,
-    name: getRandomStore(response)
-});
+    res.render("results",
+        {
+            location: user_requested.location,
+            radius: user_requested.distance,
+            price: user_requested.priceRange,
+            name: getRandomStore(response)
+        }
+    );
 }
+
+
+//helper functions 2------------------------------------------------------------------------
+
 
 function getRandomInt()
 // currently will return 0-19 since business returns 20 by default
@@ -34,13 +54,16 @@ function getRandomInt()
     return Math.floor(Math.random() * Math.floor(20));
 }
 
-//req is used if u wanna get something from the GET funciton
+
+//req is used if u wanna get something from the GET function
 //res is render shit LOL
 function getRandomStore(response)
 {
     return response.businesses[getRandomInt()].name;
 }
 
+
+//exporting main only since that's all app.js needs--------------------------------
 
 
 module.exports.main = main;
