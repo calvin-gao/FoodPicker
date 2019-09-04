c7b52b0d0b7696fcfdbbf4920b42e6daf6f24c14
Solancia <33269997+Solancia@users.noreply.github.com>
2019-09-03 22:35:04 -0700

    comments

--- public/functions.js
+++ public/functions.js
@@ -4,6 +4,7 @@ const yelp = require('yelp-fusion');
 const API_KEY = "shiSLJAHIMJ0fUf9p2LtOCHHaygchm7KSMM-KmnXTS-hHQHFaEjPasuJgphfIOzJ-PVAs-bGo0Q_5Z2IfxhZxVOLneYu6Q1mwVYYn0co6GBU1E4zeduYgUnxCl9tXXYx";
 const client = yelp.client(API_KEY);
 
+//main function that interacts with app.js; calls on yelp-funsion to make a search, in which the JSON is sent to make_page()
 async function main(req,res) {
     client.search({
         term: 'Four Barrel Coffee',
@@ -16,6 +17,7 @@ async function main(req,res) {
 
 }
 
+//renders the page, add functions that calculate from response.jsonBody here
 function make_page(req, res, response) {
     console.log();
     //req.query.userRequest to send message
