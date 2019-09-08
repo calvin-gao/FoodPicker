aa9ff69e3b1b60ebb571f3c8e206a3325ca2249e
Calvin Gao <calvig1@uci.edu>
2019-09-07 21:15:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
 // 'use strict';
-
 const yelp = require('yelp-fusion');
 const API_KEY = "shiSLJAHIMJ0fUf9p2LtOCHHaygchm7KSMM-KmnXTS-hHQHFaEjPasuJgphfIOzJ-PVAs-bGo0Q_5Z2IfxhZxVOLneYu6Q1mwVYYn0co6GBU1E4zeduYgUnxCl9tXXYx";
 const client = yelp.client(API_KEY);
